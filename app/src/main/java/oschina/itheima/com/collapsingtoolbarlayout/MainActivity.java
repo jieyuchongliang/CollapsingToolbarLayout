d6c96777635934e67e0eb936a3bcfe2e19cbaf7f
jieyuchongliang <1134119088@qq.com>
2017-06-30 11:51:46 +0800

    collapsingtoolbarlayout

---
+++ app/src/main/java/oschina/itheima/com/collapsingtoolbarlayout/MainActivity.java
@@ -0,0 +1,64 @@
+package oschina.itheima.com.collapsingtoolbarlayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        ImageView fruitImageView = (ImageView) findViewById(R.id.fruit_image_view);
+        TextView fruitContentText = (TextView) findViewById(R.id.fruit_content_text);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        collapsingToolbarLayout.setTitle("cordova");
+        fruitImageView.setImageResource(R.drawable.a);
+        String fruitContent = getContent();
+        fruitContentText.setText(fruitContent);
+    }
+
+    public String getContent() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i <800; i++) {
+            stringBuilder.append("苹果");
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                startActivity(new Intent(this,ScrollingActivity.class));
+                break;
+        }
+    }
+}
